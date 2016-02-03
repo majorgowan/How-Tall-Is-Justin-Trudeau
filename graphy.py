f1feb52ce9214d54c5c74852ce55155fa5429368
Mark Fruman <fruman@iau.uni-frankfurt.de>
2016-02-03 03:23:49 -0500

    big changes ... smaller picture, obama out, graph stats ... hope it works!

--- graphy.py
+++ graphy.py
@@ -41,10 +41,6 @@ def split_list_string(comma_delimited):
 def menu():
     return render_template('menu.html')
 
-@app.route('/questionnaire')
-def questionnaire():
-    return render_template('questionnaire.html')
-
 @app.route('/how_tall_is')
 def how_tall_is():
     return render_template('how_tall_is.html')
@@ -65,16 +61,8 @@ def how_tall_is_graph_stats():
     user_location = urllib.urlopen('http://freegeoip.net/json/' + user_ip).read()
     # survey results
     trudeau = int(request.form.get("trudeauHeightHidden"))
-    obama = int(request.form.get("obamaHeightHidden"))
     user_height = int(request.form.get("userHeightHidden"))
     user_gender = request.form["gender"]
-    # query database to get lists of past responses
-    female_data = g.db.execute('SELECT trudeau, obama FROM heights WHERE user_gender=?', ['female']).fetchall()
-    male_data = g.db.execute('SELECT trudeau, obama FROM heights WHERE user_gender=?', ['male']).fetchall()
-    print('female data')
-    print(female_data)
-    print('male data')
-    print(male_data)
     # check if ip is unique
     ip_list = g.db.execute('SELECT user_ip FROM heights').fetchall()
     if ip_list is not None:
@@ -87,108 +75,37 @@ def how_tall_is_graph_stats():
         answer_list = g.db.execute('SELECT * FROM heights WHERE user_ip=?', [user_ip]).fetchall()[0]
         answer_list = answer_list[1:]
     else:
-        answer_list = [user_ip, trudeau, obama, \
+        answer_list = [user_ip, trudeau, \
                        user_height, user_gender, user_location]
         # record results in database
         g.db.execute( \
             'INSERT INTO heights ' + \
-            '    (user_ip, trudeau, obama, ' + \
+            '    (user_ip, trudeau, ' + \
                  'user_height, user_gender, user_location) ' + \
-            'VALUES (?, ?, ?, ?, ?, ?)', \
+            'VALUES (?, ?, ?, ?, ?)', \
             answer_list)
         g.db.commit()
         print(answer_list)
         flash('Your ip: ' + str(user_ip) + '. . . Thanks for your input you lovely sod!')
+    # query database to get lists of all responses
+    female_data = g.db.execute('SELECT trudeau, user_height FROM heights WHERE user_gender=?', \
+                               ['female']).fetchall()
+    male_data = g.db.execute('SELECT trudeau, user_height FROM heights WHERE user_gender=?', \
+                               ['male']).fetchall()
+    neither_data = g.db.execute('SELECT trudeau, user_height FROM heights WHERE user_gender=?', \
+                               ['neither']).fetchall()
+    female_trudeau = [a[0] for a in female_data]
+    female_user = [a[1] for a in female_data]
+    male_trudeau = [a[0] for a in male_data]
+    male_user = [a[1] for a in male_data]
+    neither_trudeau = [a[0] for a in neither_data]
+    neither_user = [a[1] for a in neither_data]
 
     return render_template('how_tall_is_graph_stats.html', \
-            male_data=male_data, female_data=female_data, \
-            user_gender=user_gender, obama=obama, trudeau=trudeau)
-
-@app.route('/show_stats', methods=['POST'])
-def show_stats():
-    import urllib
-    # get user ip address
-    # from http://stackoverflow.com/questions/22868900/ ...
-    #                    how-do-i-safely-get-the-users-real-ip-address-in-flask-using-mod-wsgi
-    trusted_proxies = {'127.0.0.1', '127.7.30.1'}  # define your own set
-    route = request.access_route + [request.remote_addr]
-    remote_addr = next((addr for addr in reversed(route) 
-                              if addr not in trusted_proxies), request.remote_addr)
-    #user_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    user_ip = remote_addr
-    if user_ip is not None:
-        user_location = urllib.urlopen('http://freegeoip.net/json/' + user_ip).read()
-        #print('Your location is ' + user_location)
-    else:
-        user_city = None
-    # survey results
-    trudeau = int(request.form.get("trudeauHeightHidden"))
-    obama = int(request.form.get("obamaHeightHidden"))
-    user_height = int(request.form.get("userHeightHidden"))
-    user_gender = request.form["gender"]
-    # record results in database
-    # check if ip is unique
-    ip_list = g.db.execute('SELECT user_ip FROM heights').fetchall()
-    if ip_list is not None:
-        # ip_list is a list of 1-tuples, convert to list of strings
-        ip_list = [a[0] for a in ip_list]
-    # for testing purposes, don't use the ip address restriction
-    ip_list = []
-    if unicode(user_ip) in ip_list:
-        flash('Your ip: ' + str(user_ip) + ' has already answered, you sneaky sod!!')
-        answer_list = g.db.execute('SELECT * FROM heights WHERE user_ip=?', [user_ip]).fetchall()[0]
-        answer_list = answer_list[1:]
-    else:
-        answer_list = [user_ip, trudeau, obama, \
-                       user_height, user_gender, user_location]
-        g.db.execute( \
-            'INSERT INTO heights ' + \
-            '    (user_ip, trudeau, obama, ' + \
-                 'user_height, user_gender, user_location) ' + \
-            'VALUES (?, ?, ?, ?, ?, ?)', \
-            answer_list)
-        g.db.commit()
-        print(answer_list)
-        flash('Your ip: ' + str(user_ip) + '. . . Thanks for your input you lovely sod!')
-    return render_template('show_stats.html', answer_list=answer_list)
-
-@app.route('/show_questionnaire_answers', methods=['GET','POST'])
-def show_questionnaire_answers():
-    # get user ip address
-    user_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    # survey results
-    trudeau = float(request.form['trudeau'])
-    harper = float(request.form['harper'])
-    obama = float(request.form['obama'])
-    pizza = request.form.get('pizza')
-    drink = request.form.get('drink')
-    user_height = float(request.form['user_height'])
-    user_shoe = int(request.form['user_shoe'])
-    user_gender = request.form.get('user_gender')
-    # record results in database
-    # check if ip is unique
-    ip_list = g.db.execute('SELECT user_ip FROM responses').fetchall()
-    if ip_list is not None:
-        # ip_list is a list of 1-tuples, convert to list of strings
-        ip_list = [a[0] for a in ip_list]
-    # for testing purposes, don't use the ip address restriction
-    ip_list = []
-    if unicode(user_ip) in ip_list:
-        flash('Your ip: ' + str(user_ip) + ' has already answered, you sneaky sod!!')
-        answer_list = g.db.execute('SELECT * FROM responses WHERE user_ip=?', [user_ip]).fetchall()[0]
-        answer_list = answer_list[1:]
-    else:
-        answer_list = [user_ip, trudeau, harper, obama, pizza, drink, \
-                       user_height, user_shoe, user_gender]
-        g.db.execute( \
-            'INSERT INTO responses ' + \
-            '    (user_ip, trudeau, harper, obama, pizza, drink, ' + \
-                 'user_height, user_shoe, user_gender) ' + \
-            'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', \
-            answer_list)
-        g.db.commit()
-        flash('Your ip: ' + str(user_ip) + '. . . Thanks for your input you lovely sod!')
-    return render_template('show_questionnaire_answers.html', answer_list=answer_list)
+            male_trudeau=male_trudeau, male_user=male_user, \
+            female_trudeau=female_trudeau, female_user=female_user, \
+            neither_trudeau=neither_trudeau, neither_user=neither_user, \
+            user_gender=user_gender, user_height=user_height, trudeau=trudeau)
 
 if __name__ == '__main__':
     app.run()
