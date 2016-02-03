6e360673096dcce91d7b213ea2e32925c4563d56
Mark Fruman <fruman@iau.uni-frankfurt.de>
2016-02-03 17:02:46 -0500

    new obama and merkel versions and more generic (reusable) code

--- graphy.py
+++ graphy.py
@@ -41,18 +41,18 @@ def split_list_string(comma_delimited):
 def menu():
     return render_template('menu.html')
 
-@app.route('/how_tall_is')
-def how_tall_is():
-    return render_template('how_tall_is.html')
+@app.route('/how_tall_is/<name>')
+def how_tall_is(name):
+    return render_template('how_tall_is.html',name=name)
 
-@app.route('/how_tall_is_graph_stats', methods=['POST'])
-def how_tall_is_graph_stats():
+@app.route('/how_tall_is_graph_stats/<name>', methods=['POST'])
+def how_tall_is_graph_stats(name):
     import urllib
     # get user ip address
     # from http://stackoverflow.com/questions/22868900/ ...
     #                    how-do-i-safely-get-the-users-real-ip-address-in-flask-using-mod-wsgi
     trusted_proxies = {'127.0.0.1', '127.7.30.1'}  # define your own set
-    # trusted_proxies = []
+    trusted_proxies = []
     route = request.access_route + [request.remote_addr]
     remote_addr = next((addr for addr in reversed(route) 
                               if addr not in trusted_proxies), request.remote_addr)
@@ -60,11 +60,11 @@ def how_tall_is_graph_stats():
     # get user location
     user_location = urllib.urlopen('http://freegeoip.net/json/' + user_ip).read()
     # survey results
-    trudeau = int(request.form.get("trudeauHeightHidden"))
+    height = int(request.form.get("heightHidden"))
     user_height = int(request.form.get("userHeightHidden"))
     user_gender = request.form["gender"]
     # check if ip is unique
-    ip_list = g.db.execute('SELECT user_ip FROM heights').fetchall()
+    ip_list = g.db.execute('SELECT user_ip FROM ' + name).fetchall()
     if ip_list is not None:
         # ip_list is a list of 1-tuples, convert to list of strings
         ip_list = [a[0] for a in ip_list]
@@ -72,15 +72,16 @@ def how_tall_is_graph_stats():
     ip_list = []
     if unicode(user_ip) in ip_list:
         flash('Your ip: ' + str(user_ip) + ' has already answered, you sneaky sod!!')
-        answer_list = g.db.execute('SELECT * FROM heights WHERE user_ip=?', [user_ip]).fetchall()[0]
+        answer_list = g.db.execute('SELECT * FROM ' + \
+                                   name + ' WHERE user_ip=?', [user_ip]).fetchall()[0]
         answer_list = answer_list[1:]
     else:
-        answer_list = [user_ip, trudeau, \
+        answer_list = [user_ip, height, \
                        user_height, user_gender, user_location]
         # record results in database
         g.db.execute( \
-            'INSERT INTO heights ' + \
-            '    (user_ip, trudeau, ' + \
+            'INSERT INTO ' + name + ' ' + \
+            '    (user_ip, height, ' + \
                  'user_height, user_gender, user_location) ' + \
             'VALUES (?, ?, ?, ?, ?)', \
             answer_list)
@@ -88,24 +89,24 @@ def how_tall_is_graph_stats():
         print(answer_list)
         flash('Your ip: ' + str(user_ip) + '. . . Thanks for your input you lovely sod!')
     # query database to get lists of all responses
-    female_data = g.db.execute('SELECT trudeau, user_height FROM heights WHERE user_gender=?', \
+    female_data = g.db.execute('SELECT height, user_height FROM ' + name + ' WHERE user_gender=?', \
                                ['female']).fetchall()
-    male_data = g.db.execute('SELECT trudeau, user_height FROM heights WHERE user_gender=?', \
+    male_data = g.db.execute('SELECT height, user_height FROM ' + name + ' WHERE user_gender=?', \
                                ['male']).fetchall()
-    neither_data = g.db.execute('SELECT trudeau, user_height FROM heights WHERE user_gender=?', \
+    neither_data = g.db.execute('SELECT height, user_height FROM ' + name + ' WHERE user_gender=?', \
                                ['neither']).fetchall()
-    female_trudeau = [a[0] for a in female_data]
+    female_height = [a[0] for a in female_data]
     female_user = [a[1] for a in female_data]
-    male_trudeau = [a[0] for a in male_data]
+    male_height = [a[0] for a in male_data]
     male_user = [a[1] for a in male_data]
-    neither_trudeau = [a[0] for a in neither_data]
+    neither_height = [a[0] for a in neither_data]
     neither_user = [a[1] for a in neither_data]
 
-    return render_template('how_tall_is_graph_stats.html', \
-            male_trudeau=male_trudeau, male_user=male_user, \
-            female_trudeau=female_trudeau, female_user=female_user, \
-            neither_trudeau=neither_trudeau, neither_user=neither_user, \
-            user_gender=user_gender, user_height=user_height, trudeau=trudeau)
+    return render_template('how_tall_is_graph_stats.html', name=name, \
+            male_height=male_height, male_user=male_user, \
+            female_height=female_height, female_user=female_user, \
+            neither_height=neither_height, neither_user=neither_user, \
+            user_gender=user_gender, user_height=user_height, height=height)
 
 if __name__ == '__main__':
     app.run()
--- schema.sql
+++ schema.sql
@@ -1,8 +1,28 @@
-DROP TABLE IF EXISTS heights;
-CREATE TABLE heights (
+DROP TABLE IF EXISTS trudeau;
+CREATE TABLE trudeau (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_ip TEXT NOT NULL,
-    trudeau INTEGER NOT NULL,
+    height INTEGER NOT NULL,
+    user_height INTEGER NOT NULL,
+    user_gender TEXT NOT NULL,
+    user_location TEXT
+);
+
+DROP TABLE IF EXISTS obama;
+CREATE TABLE obama (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_ip TEXT NOT NULL,
+    height INTEGER NOT NULL,
+    user_height INTEGER NOT NULL,
+    user_gender TEXT NOT NULL,
+    user_location TEXT
+);
+
+DROP TABLE IF EXISTS merkel;
+CREATE TABLE merkel (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_ip TEXT NOT NULL,
+    height INTEGER NOT NULL,
     user_height INTEGER NOT NULL,
     user_gender TEXT NOT NULL,
     user_location TEXT
